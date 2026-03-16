f690f19d572a6e98066e9daaf2c5a400212f9b7d
savagegroupspa-png <savagegroupspa@gmail.com>
2026-03-16 20:38:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const { connectDB } = require('./config/db');
 const { swaggerDocs } = require('./config/swagger');
 
 // Importar rutas
