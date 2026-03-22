efe19ad8c83a8c3ed150340ca18d2a522b506057
savagegroupspa-png <savagegroupspa@gmail.com>
2026-03-22 19:22:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,5 +25,5 @@ app.get('/', (req, res) => res.send('🚀 Servidor Savage operando'));
 const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`🚀 Servidor Savage corriendo en puerto ${PORT}`);
 });
